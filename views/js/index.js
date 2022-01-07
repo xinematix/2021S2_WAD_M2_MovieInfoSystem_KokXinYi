a53cc4e3471df214afbd20e36347dd75d2d782af
Agnes0808 <75418798+Agnespkt@users.noreply.github.com>
2022-01-07 23:43:30 +0800

    view movie details

--- views/js/index.js
+++ views/js/index.js
@@ -1,57 +1,77 @@
-$(function () {
-	const api_key = "api_key=95efee1e5cec02671e7e3c3d11b4065c"
-	const baseURL = "https://api.themoviedb.org/3/movie/";
-	var api_url = baseURL + "popular?" + api_key;
-	const searchURL = 'https://api.themoviedb.org/3/search/movie?' + api_key;
+const api_key = "api_key=95efee1e5cec02671e7e3c3d11b4065c"
+const baseURL = "https://api.themoviedb.org/3/movie/";
+var api_url = baseURL + "popular?" + api_key;
+const searchURL = 'https://api.themoviedb.org/3/search/movie?' + api_key;
+// const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
-	const form = document.getElementById('searchForm');
-	const search = document.getElementById('search');
-	const main = document.getElementById('main');
+const main = document.getElementById('main');
+const form =  document.getElementById('form');
+const search = document.getElementById('search');
 
-	// $.getJSON( api_url, function( data ) {
-	//     $.each( data.results, function( i, item ) {
-	//         var posterFullUrl = "https://image.tmdb.org/t/p/w185//" + item.poster_path;
-	//         $("<div class='movie-card'><img src='" + posterFullUrl + "'><h3>" + item.title + "</h3></div>").appendTo(".movies");
-	//     });
-	// });
+getMovies(api_url);
 
-	getMovies(api_url);
+function getMovies(url) {
+  lastUrl = url;
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        if(data.results.length !== 0){
+            showMovies(data.results);
+        }else{
+            main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
+        } 
+    })
+}
 
-	function getMovies(url) {
-		lastUrl = url;
-		fetch(url).then(res => res.json()).then(data => {
-			console.log(data.results);
-			if (data.results.length !== 0) {
-				showMovies(data.results);
-			} else {
-				main.innerHTML = `<h1 class="no-results">No Results Found</h1>`
-			}
-		})
-	}
+function showMovies(data) {
+    main.innerHTML = '';
+    data.forEach(item => {
+        const {title, poster_path, id} = item;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        var posterFullUrl = "https://image.tmdb.org/t/p/w185//" + poster_path;
+        movieEl.innerHTML = `<img src="${posterFullUrl}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+            </div>
+                <button class="know-more" id="${id}">Know More</button>`
+        main.appendChild(movieEl);
+        document.getElementById(id).addEventListener('click', () => {
+          console.log(id)
+          openNav(item)
+        })
+    })
+}
 
-	function showMovies(data) {
-		main.innerHTML = '';
-		data.forEach(item => {
-			const movieEl = document.createElement('div');
-			movieEl.classList.add('movie');
-			var posterFullUrl = "https://image.tmdb.org/t/p/w185//" + item.poster_path;
-			var moviedetails = "https://api.themoviedb.org/3/movie/" + item.id+'?' + api_key;
-			movieEl.innerHTML = `
-        <div class='movie-card'><img src='${posterFullUrl}'><h3><a href=${moviedetails}> ${item.title} </a></h3>
-        </div>`
-			main.appendChild(movieEl);
-		})
-	}	
+const overlayContent = document.getElementById('overlay-content');
+function openNav(item) {
+  let id = item.id;
+  fetch(baseURL +id+'/videos?'+api_key).then(res => res.json()).then(videoData => {
+    console.log(videoData);
+    if(videoData){
+      document.getElementById("myNav").style.width = "100%";
+      if(videoData.results.length > 0){
+        var content = `<p>Movie Title: ${item.original_title}</p>
+        <p>Description</p>
+        <p>${item.overview}</p>
+        <br/>`
+        overlayContent.innerHTML = content;
+      }else{
+        overlayContent.innerHTML = `<h1 class="no-results">No Results Found</h1>`
+      }
+    }
+  })
+}
 
-	form.addEventListener('submit', (e) => {
-		e.preventDefault();
-		const searchTerm = search.value;
-		if (searchTerm) {
-			getMovies(searchURL + '&query=' + searchTerm)
-		} else {
-			getMovies(api_url);
-		}
-	})
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
 
-	
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const searchTerm = search.value;
+    if(searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(api_url);
+    }
 })
